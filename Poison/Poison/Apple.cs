dfa00885f7647bf9bcce8fef448694cc972eb967
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2018-06-05 17:30:14 +0100

    Added code block and apple class. Produced 10000 apples

---
+++ Poison/Poison/Apple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poison
+{
+    class Apple
+    {
+        public string Colour { get; set; }
+        public bool Poisoned { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Colour} apple{(Poisoned ? " (poisoned!)" : "")}";
+        }
+    }
+}
--- Poison/Poison/Program.cs
+++ Poison/Poison/Program.cs
@@ -4,12 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Poison
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var apples = PickApples().Take(100000);
+
+            foreach (var apple in apples)
+            {
+                Console.WriteLine(apple.Colour + "  " + apple.Poisoned);
+            }
+
+            Console.ReadLine();
+
+             IEnumerable<Apple> PickApples()
+            {
+                int colourIndex = 1;
+                int poisonIndex = 7;
+
+                while (true)
+                {
+                    yield return new Apple
+                    {
+                        Colour = GetColour(colourIndex),
+                        Poisoned = poisonIndex % 41 == 0
+                    };
+
+                    colourIndex += 5;
+                    poisonIndex += 37;
+                }
+            }
+
+            string GetColour(int colourIndex)
+            {
+                if (colourIndex % 13 == 0 || colourIndex % 29 == 0)
+                {
+                    return "Green";
+                }
+
+                if (colourIndex % 11 == 0 || colourIndex % 19 == 0)
+                {
+                    return "Yellow";
+                }
+
+                return "Red";
+            }
+
+        
         }
+
     }
+
 }
+
+
+
+
